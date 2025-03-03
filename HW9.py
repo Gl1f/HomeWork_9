f97988ff91a08ce0ed902a86f80a321447634ed8
Gl1f <k.max2517@gmail.com>
2025-03-03 13:08:51 +0300

    Добавление фильтрации айди по числам

--- HW9.py
+++ HW9.py
@@ -1,3 +1,4 @@
 from marvel import full_dict
 
-
+films_id: list[str] = input(' Введите ID фильмов через пробел: ').split()
+filtered_films_id: list[int] = list(map(lambda x: int(x) if x.isdigit() else None, films_id))
