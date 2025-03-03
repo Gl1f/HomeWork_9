0c79f0e8f8a2e30bfafa6a721fbfb21e1dbdd256
Gl1f <k.max2517@gmail.com>
2025-03-03 13:32:27 +0300

    Сделал 2-3 задания

--- HW9.py
+++ HW9.py
@@ -1,4 +1,14 @@
 from marvel import full_dict
 
+# 1 Задание
 films_id: list[str] = input(' Введите ID фильмов через пробел: ').split()
 filtered_films_id: list[int] = list(map(lambda x: int(x) if x.isdigit() else None, films_id))
+print(filtered_films_id)
+
+# 2 Задание
+filtered_films: dict[any, any] = dict(filter(lambda x: x[0] in filtered_films_id, full_dict.items()))
+print(filtered_films)
+
+# 3 Задание
+set_directors: set[str] = {film.get('director') for film in filtered_films.values()}
+print(set_directors)
